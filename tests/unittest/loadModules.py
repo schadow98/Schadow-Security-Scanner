a5272918e4a54b0c0de393c4cf303e2414c92cbc
schadow98 <malteschadow@yahoo.de>
2024-07-17 18:50:55 +0200

    Update

--- tests/unittest/loadModules.py
+++ tests/unittest/loadModules.py
@@ -6,6 +6,7 @@ fileDir = os.path.dirname(os.path.abspath(__file__))
 PROJECT_PATH = os.path.abspath(os.path.join(fileDir, "..", ".."))
 os.environ["PROJECT_PATH"] = PROJECT_PATH
 
+print(sys.path)
 
 SOURCE_PATH = os.path.join(
     PROJECT_PATH, "src"
