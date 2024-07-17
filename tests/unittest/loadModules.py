01a984795ca742113a584648784ca91b1b26bff4
schadow98 <malteschadow@yahoo.de>
2024-07-17 18:48:41 +0200

    Fix

--- tests/unittest/loadModules.py
+++ tests/unittest/loadModules.py
@@ -5,21 +5,20 @@ import sys
 fileDir = os.path.dirname(os.path.abspath(__file__))
 PROJECT_PATH = os.path.abspath(os.path.join(fileDir, "..", ".."))
 os.environ["PROJECT_PATH"] = PROJECT_PATH
+
 SOURCE_PATH = os.path.join(
     PROJECT_PATH, "src", "tools", "logger"
 )
-
+sys.path.append(SOURCE_PATH)
 SOURCE_PATH = os.path.join(
     PROJECT_PATH, "src", "tools"
 )
-
+sys.path.append(SOURCE_PATH)
 SOURCE_PATH = os.path.join(
     PROJECT_PATH, "src"
 )
-
-
-
 sys.path.append(SOURCE_PATH)
+
 TEST_PATH = os.path.join(
     PROJECT_PATH, "tests"
 )
