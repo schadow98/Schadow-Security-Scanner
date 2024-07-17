491cb367aecaa37bbbb37777bb82d66b2a1ebbb2
schadow98 <malteschadow@yahoo.de>
2024-07-17 17:16:01 +0200

    Debug messages

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -l
 
+pwd
 ls -alt
-cwd
+echo "~"
 ls -alt ~
+echo "dist"
 ls -alt /dist
+echo "work"
 ls -alt /work
 
 /dist/SecurityScannerSchadow --configFile /dist/securityScannerConfig.json --work /work
